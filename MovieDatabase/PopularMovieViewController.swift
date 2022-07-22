7f99326e079209c0e41ba44556b6f9badaccd6fb
maksimturkin <maksimturkin50@gmail.com>
2022-07-22 21:57:18 +0300

    correct work of search bar

--- MovieDatabase/PopularMovieViewController.swift
+++ MovieDatabase/PopularMovieViewController.swift
@@ -4,30 +4,45 @@
 //
 //  Created by Maksim Turkin on 22.07.2022.
 //
-
+ 
 import UIKit
 
-class PopularMovieViewController: UIViewController {
-
-    @IBOutlet var mainView: UIView!
-    @IBOutlet weak var searchBar: UISearchBar!
+class PopularMovieViewController: UIViewController, UISearchResultsUpdating {
     
+    let searchController = UISearchController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.searchController = searchController
+        self.searchController.searchResultsUpdater = self
+        setTitle()
         setSearchBarStyle()
         
     }
     
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else {
+            return
+        }
+        print(text)
+    }
+    
     private func setSearchBarStyle() {
-        if let textfield = searchBar.value(forKey: "searchField") as? UITextField {
-            textfield.backgroundColor = UIColor(named: "BackgroundColor")
+        if let textfield = searchController.searchBar.value(forKey: "searchField") as? UITextField {
             textfield.textColor = UIColor.white
+            textfield.placeholder = "Please enter movie name"
         }
     }
     
-
+    private func setTitle() {
+        let label = UILabel()
+        label.textColor = UIColor.white
+        label.text = "Search movie".uppercased()
+        label.font = UIFont.boldSystemFont(ofSize: 25.0)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: label)
+    }
+    
 }
 
 
